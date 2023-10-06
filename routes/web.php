c9eb426736310c74b79c26190982665b7024a829
Mauro Piccione <mauro.piccione@student.unife.it>
2023-10-06 17:00:03 +0200

    Risolto route

--- routes/web.php
+++ routes/web.php
@@ -29,15 +29,12 @@ use App\Http\Controllers\PrenotazioneController;
 
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware(['auth:web'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-   
+    Route::get('/dashboard', [ProdottoController::class, 'index'])->name('dashboard');
 });
 
 
