aa7be517e64c3353702bdb485d4ed52fd97f6ac4
SofiaBuriani25 <sofia.buriani@edu.unife.it>
2023-10-10 09:58:52 +0200

    barra di ricerca

--- resources/views/daOrdinare.blade.php
+++ resources/views/daOrdinare.blade.php
@@ -23,7 +23,7 @@
                     </div>
                    
                     <br>
-                    <table class="min-w-full divide-y divide-gray-200">
+                    <table class="min-w-full divide-y divide-gray-200" id="table">
                         <thead class="bg-gray-50 dark:bg-gray-900">
                             <tr>
                                 <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
