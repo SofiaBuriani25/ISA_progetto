95a5fac4ee33104bd0f9eb9688e2e6d7460dfde4
Mauro Piccione <mauro.piccione@student.unife.it>
2023-10-10 10:58:42 +0200

    Test dipendneti

--- resources/views/dipendente_home.blade.php
+++ resources/views/dipendente_home.blade.php
@@ -127,7 +127,7 @@
         <input type="hidden" name="prodotto_id" value="{{ $prodotto->id }}">
                                         <input type="hidden" name="azione" value="aggiungi">
                                         <button class="ml-3" type="submit">
-                                            Aggiungi
+                                            Aggiungi prodotto
                                         </button>
         </div>
     </form>
